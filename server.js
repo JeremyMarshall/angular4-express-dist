3ed96de75722a73974b8c3bf86a079f7750a290b
JeremyMarshall <jeremystuartmarshall@gmail.com>
2017-10-12 13:46:14 +1100

    initial

---
+++ server.js
@@ -0,0 +1,47 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+// Get our API routes
+const api = require('./server/routes/api');
+const xterm = require('./server/routes/terminals');
+
+const app = express();
+
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '.')));
+
+// Set our api routes
+app.use('/api', api);
+
+app.use('/terminals', xterm);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+var expressWs = require('express-ws')(app, server);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
